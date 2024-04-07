6eb56c82d9fdf0cfe3c9837a31ca9397f0cef063
Lincoln Ruteski <lruteski@gmail.com>
2024-04-07 11:20:26 -0300

    pacote rotas e controllers

---
+++ api/src/router/rotas/rotas.go
@@ -0,0 +1,11 @@
+package rotas
+
+import "net/http"
+
+// representa todas as rotas da api
+type Rota struct {
+	Uri                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
+	RequerAutenticacao bool
+}
